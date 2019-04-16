1da8699e7a96856171fac096a97e67e0b682ce50
Alessio Zap Boerio <boerioalessio@gmail.com>
2019-04-16 16:45:02 +0200

    Create xcodeproj

---
+++ AnimeRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimeRate
+//
+//  Created by Alessio Boerio on 16/04/2019.
+//  Copyright © 2019 zapideas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
